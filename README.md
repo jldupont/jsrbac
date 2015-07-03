a687a4364bcd726eb664d3e58ef43ae7e617fe9f
github@jldupont.com <github@jldupont.com>
2015-07-03 17:30:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Description
+-----------
+
+Javascript library for the implementation of role based access control in a web application.
+
+
+WORK IN PROGRESS
+================
---
+++ jsrbac.js
@@ -0,0 +1,15 @@
+/*
+ * jsrbac.js  
+ * 
+ * Javascript library for RBAC (Role Based Access Control)
+ * 
+ * @author Jean-Lou Dupont 
+ * 
+ */
+
+window.rbac = window.rbac || {};
+
+(function(){
+	
+	
+})();
